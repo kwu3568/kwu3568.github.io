a3c6d3e23e7aecd4ba01a74b86c84979e0e1e83c
Kevin Wu <kevinwu@Kevins-Air.home>
2024-02-20 16:12:28 -0500

    base index

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <!DOCTYPE html>
+   <html>
+   <body>
+   <h1 id = "header">Kevin Wu</h1>   
+   </body>
+   </html>
   );
 }
 
