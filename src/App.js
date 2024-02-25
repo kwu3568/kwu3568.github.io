3fc085a41adc1d107c199245369266e00e35936e
Kevin Wu <kevinwu@Kevins-Air.home>
2024-02-25 10:16:25 -0500

    website changes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 
 function App() {
   return (
-   <!DOCTYPE html>
    <html>
    <body>
-   <h1 id = "header">Kevin Wu</h1>   
+   <h1 id = "header">Kevin Wu</h1>
+   <div class = "contact_info">
+   <div class = "email">kwu3568@gmail.com</div>
+   <div class = "phone_number">(917)-968-3876</div>
+   </div>   
    </body>
    </html>
   );
