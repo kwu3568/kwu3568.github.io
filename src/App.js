ae2964265fa9271dcf701fa7a11544a87d339f9a
Kevin Wu <kevinwu@Kevins-Air.home>
2024-03-02 10:48:08 -0500

    added links

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
    <div class = "contact_info">
    <div class = "email">kwu3568@gmail.com</div>
    <div class = "phone_number">(917)-968-3876</div>
+   </div>
+   <div class = "links">
+   <a href = "https://github.com/kwu3568">
+   <img class = "github" src = "github.png"></img>
+   </a>
+   <a href = "https://www.linkedin.com/in/kevin-wu-871287118">
+   <img class = "linkedin" src = "linkedin.png"></img>
+   </a>
    </div>   
    </body>
    </html>
