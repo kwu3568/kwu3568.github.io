a04009126f6285d579f78b94dd53dec83ee8a1c4
Kevin Wu <kevinwu@Kevins-Air.home>
2024-03-11 11:53:16 -0400

    added resume

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
+class App extends React.Component{
+
+  constructor(props){
+  	super(props);
+  	this.state = {
+  		flag: 0
+  	}
+  	
+  }
+  show_resume(){
+  	if(this.state.flag == 0){
+  		document.getElementById("resume_pdf").style.display = "inline-block";
+  		this.setState({
+  			flag: 1
+  		})
+  	}
+  	else{
+  		document.getElementById("resume_pdf").style.display = "none";
+  		this.setState({
+  			flag: 0
+  		})
+  	}
+  }
+  renderList(){
   return (
    <html>
    <body>
    <h1 id = "header">Kevin Wu</h1>
+   <div class = "resume_container">
+   <img id = "resume_pdf" class = "resume_pdf" src = "resume.pdf"></img>
+   </div>
    <div class = "contact_info">
    <div class = "email">kwu3568@gmail.com</div>
    <div class = "phone_number">(917)-968-3876</div>
@@ -17,10 +43,17 @@ function App() {
    <a href = "https://www.linkedin.com/in/kevin-wu-871287118">
    <img class = "linkedin" src = "linkedin.png"></img>
    </a>
+   <img onClick = {() => this.show_resume()} class = "resume" src = "resume.jpeg"></img>
    </div>   
    </body>
    </html>
   );
+}
+ render(){
+ 	return(
+ 		this.renderList()
+ 	)
+ }
 }
 
 export default App;
