07d1e87c8bfbe10997fcc03b240047e7a7cad418
Kevin Wu <kevinwu@Kevins-Air.home>
2024-03-14 10:18:17 -0400

    added hooks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,50 @@
 import './App.css';
-import React from 'react';
+import React ,{useState} from 'react';
 
+export default function App(){
+	const[flag, setFlag] = useState(0);
+	
+	function show_resume(){
+		if(flag === 0){
+			document.getElementById("resume_png").style.display = "inline-block";
+			setFlag(1);
+			
+		}
+		else{
+			document.getElementById("resume_png").style.display = "none";
+			setFlag(0);
+			
+		}  	
+	}
+
+	return(
+		<>
+		<html>
+		   <body>
+		   <h1 id = "header">Kevin Wu</h1>
+		   <div class = "resume_container">
+		   <img onClick = {() => show_resume()} id = "resume_png" class = "resume_png" src = "resume.png" alt ="resume"></img>
+		   </div>
+		   <div class = "contact_info">
+		   <div class = "email">kwu3568@gmail.com</div>
+		   <div class = "phone_number">(917)-968-3876</div>
+		   </div>
+		   <div class = "links">
+		   <a href = "https://github.com/kwu3568" target= "_blank">
+		   <img class = "github" src = "github.png" alt="github"></img>
+		   </a>
+		   <a href = "https://www.linkedin.com/in/kevin-wu-871287118" target= "_blank">
+		   <img class = "linkedin" src = "linkedin.png" alt="linkedin"></img>
+		   </a>
+		   <img onClick = {() => show_resume()} class = "resume" src = "resume.jpeg" alt="resume"></img>
+		   </div>   
+		   </body>
+		   </html>
+		</>
+	)
+}	
+
+/*
 class App extends React.Component{
 
   constructor(props){
@@ -57,3 +101,4 @@ class App extends React.Component{
 }
 
 export default App;
+*/
